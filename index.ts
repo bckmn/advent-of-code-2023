3e5519e44eb349c81ffb280a6a9c65a9c839e45d
Simon Bäckman <simon@bckmn.se>
2023-11-30 22:09:07 +0100

    Setup cli program for running solution files

--- index.ts
+++ index.ts
@@ -1,1 +1,24 @@
-console.log("Hello via Bun!");
+import { Command } from "commander";
+import { resolve } from "path";
+import { padWithZero } from "./utils";
+
+const program = new Command("aoc");
+
+program
+  .command("run")
+  .description("Run day solution")
+  .argument("<day:number>", "Day to run")
+  .option("-p, --part <part:number>", "Part of the day solution to run", "1")
+  .option(
+    "-f, --file <file:string>",
+    "Input file. If missing, the day input file is used instead",
+    "input.txt",
+  )
+  .action(async (day, { part, file }) => {
+    const path = resolve(import.meta.dir, `days/${padWithZero(day)}`);
+    const { runSolution } = require(path);
+    const text = await Bun.file(resolve(path, file)).text();
+    console.log("solution:", runSolution(text, part === 2));
+  });
+
+program.parse();
---
+++ utils.ts
@@ -0,0 +1,1 @@
+export const padWithZero = (num: number) => String(num).padStart(2, "0");
