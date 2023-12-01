0e011102b791380c462dfb2d5438bd78b498e341
Simon Bäckman <simon@bckmn.se>
2023-12-01 06:52:59 +0100

    Initial solution for day one

--- days/01/partOne.ts
+++ days/01/partOne.ts
@@ -1,3 +1,10 @@
-export const partOne = (input: number[]) => {
-  return 0;
+export const partOne = (input: string[]) => {
+  return input.reduce((acc, curr) => {
+    const numbers = curr.split("").map(l => !isNaN(Number(l)) ? l : "").join("");
+    const firstNumber = numbers.at(0) ?? "";
+    const secondNumber = numbers.at(-1) ?? "";
+    return secondNumber === "" ?
+      acc
+      : acc + Number(firstNumber + secondNumber);
+    }, 0);
 };
