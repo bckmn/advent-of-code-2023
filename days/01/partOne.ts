1c45ac6b044f680568cb78162990ad0c9b94d930
Simon Bäckman <simon@bckmn.se>
2023-12-01 10:19:58 +0100

    Refactor partOne of day 1

--- days/01/partOne.ts
+++ days/01/partOne.ts
@@ -1,10 +1,11 @@
 export const partOne = (input: string[]) => {
   return input.reduce((acc, curr) => {
-    const numbers = curr.split("").map(l => !isNaN(Number(l)) ? l : "").join("");
+    const numbers = curr
+      .split("")
+      .map((l) => (!isNaN(Number(l)) ? l : ""))
+      .join("");
     const firstNumber = numbers.at(0) ?? "";
     const secondNumber = numbers.at(-1) ?? "";
-    return secondNumber === "" ?
-      acc
-      : acc + Number(firstNumber + secondNumber);
-    }, 0);
+    return secondNumber === "" ? acc : acc + Number(firstNumber + secondNumber);
+  }, 0);
 };
